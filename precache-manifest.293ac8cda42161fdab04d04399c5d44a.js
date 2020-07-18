0a96e629d75ff7b143b4d61f04b7eb72ad9cda38
Samul <samulshrestha101@gmail.com>
2020-07-18 12:37:21 +0545

    Updates

---
+++ precache-manifest.293ac8cda42161fdab04d04399c5d44a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c02255552a43209b3b6c1f375538c7f",
+    "url": "/portfoliowebsite/index.html"
+  },
+  {
+    "revision": "b6b845cb4730f2290d11",
+    "url": "/portfoliowebsite/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "fd4ec8002f89ec3f2b8d",
+    "url": "/portfoliowebsite/static/css/main.71d4cc66.chunk.css"
+  },
+  {
+    "revision": "b6b845cb4730f2290d11",
+    "url": "/portfoliowebsite/static/js/2.ff05887f.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfoliowebsite/static/js/2.ff05887f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd4ec8002f89ec3f2b8d",
+    "url": "/portfoliowebsite/static/js/main.8a83ced4.chunk.js"
+  },
+  {
+    "revision": "b0e11731550543164d59",
+    "url": "/portfoliowebsite/static/js/runtime-main.3cc09347.js"
+  }
+]);
